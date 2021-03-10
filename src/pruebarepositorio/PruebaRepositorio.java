98bd909962646e1e682bf80940cb467a0849a691
Massielle Coti <Massielle Coti@192.168.0.165>
2021-03-10 17:29:50 -0600

    holaaaaaa

---
+++ src/pruebarepositorio/PruebaRepositorio.java
@@ -0,0 +1,18 @@
+
+package pruebarepositorio;
+
+/**
+ *
+ * @author Massielle Coti
+ */
+public class PruebaRepositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Holi");
+        System.out.println("Si me sale esto bien me como unos cheetos");
+    }
+    
+}
